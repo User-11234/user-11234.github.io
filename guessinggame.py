c8cd1e186266aaaf59f231d3684d342747981256
Julian Hoque <julianhoq.31159@redlandsschools.net>
2018-11-01 14:51:02 +0000

    Adrianwas here lomawosa

---
+++ guessinggame.py
@@ -0,0 +1,32 @@
+import random
+
+gameLose = False
+lives = 5
+print ("A number has been selected between one and one-hundred.")
+x = random.randint(1,100)
+
+while (gameLose == False):
+
+  ans = int(input("What is your guess? "))
+
+  if ans > (x):
+    print ("This number is too high!")
+
+  if ans < (x):
+    print ("This number is too low...")
+  
+  if ans == (x):
+    print ("You win!")
+    gameLose = True
+  
+  if (lives == 0):
+    print ("You lose!")
+    gameLose = True
+
+  if (ans == "ValueError"): 
+    print ("Please input a number!")
+
+  if (ans > 100) or (ans < 1) :
+    print ("This is not within the range.")
+
+  lives = (lives - 1)
